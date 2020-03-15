10efc8d5703923087499ff962dced95966d9a4f9
Ivan Sushkov <48153970+fuckthegoose@users.noreply.github.com>
2020-03-15 23:00:54 +0700

    heroku settings

--- testapp/settings.py
+++ testapp/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 from django.urls import reverse_lazy
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +136,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = reverse_lazy('signform:login')
 LOGIN_REDIRECT_URL = '/auth/index/'
 LOGOUT_REDIRECT_URL = '/auth/login/'
+
+
+django_heroku.settings(locals())
