3c8a251ef1aee1b33f46dc5fa72146eb20e544b3
Ivan Sushkov <48153970+fuckthegoose@users.noreply.github.com>
2020-03-16 09:29:14 +0700

    auth.forms
    Переделал на готовых формах

--- signform/views.py
+++ signform/views.py
@@ -1,16 +1,14 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from django.utils.text import slugify
 from django.views import View
 
 from django.contrib.auth import login
 from django.contrib.auth import logout
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import AccessMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import SignUpForm, LogInForm
+from .forms import UserCreationFormWithSubscribe
 from .models import Subscribers
 
 
@@ -26,56 +24,35 @@ class SignUpView(NotLoginRequiredMixin, View):
     template_name = 'signform/forms/signup.html'
 
     def post(self, request):
-        form = SignUpForm(request.POST)
+        form = UserCreationFormWithSubscribe(data=request.POST)
         if form.is_valid():
-            self.create_user(form.cleaned_data)
+            user = form.save()
+
+            if form.cleaned_data.get('subscribe_news', False):
+                Subscribers.objects.create(user=user)
 
             return redirect(reverse('signform:login'))
 
         return render(request, self.template_name, {'form': form})
 
     def get(self, request):
-        form = SignUpForm()
+        form = UserCreationFormWithSubscribe()
         return render(request, self.template_name, {'form': form})
-
-    def create_user(self, data):
-        user = User.objects.create_user(
-            data['username'],
-            data['email'],
-            data['password'],
-            **{
-                'first_name': data['first_name'],
-                'last_name': data['last_name'],
-                'is_active': True
-            }
-        )
-
-        if data.get('subscribe_news', False):
-            Subscribers.objects.create(user=user)
-
-        return user
 
 
 class LoginView(NotLoginRequiredMixin, View):
-    errors = []
     template_name = 'signform/forms/login.html'
 
     def get(self, request):
-        form = LogInForm()
-        return render(request, self.template_name, {'form': form, 'errors': self.errors})
+        form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        form = LogInForm(request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'],
-                                password=form.cleaned_data['password'])
-
-            if user is None:
-                self.errors.append('Invalid credentials')
-            else:
-                login(request, user)
-                return redirect(reverse('signform:index'))
-        return render(request, self.template_name, {'form': form, 'errors': self.errors})
+            login(request, form.get_user())
+            return redirect(reverse('signform:index'))
+        return render(request, self.template_name, {'form': form})
 
 
 class LogoutView(LoginRequiredMixin, View):
